b2587236c94206284cb6a73e9c33e5156ad850dd
Sung-Hyun-Kim <kksscc4@gmail.com>
2021-01-25 17:45:13 +0900

    1로 만들기

---
+++ DP/P1463/P1463.py
@@ -0,0 +1,21 @@
+from sys import stdin
+
+stdin = open('./input.txt', 'r')
+
+num = int(stdin.readline())
+
+# dp 테이블
+d = [0] * 1000001
+
+# 2부터 num까지 d[i]계산
+for i in range(2, num + 1):
+    d[i] = d[i-1] + 1
+
+    # 3으로 나눠 떨어진다면
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+    # 5로 나눠 떨어진다면
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i // 3] + 1)
+
+print(d[num])
