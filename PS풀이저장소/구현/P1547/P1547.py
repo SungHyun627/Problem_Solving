8fc7c297879aa587039ed8adde3f4e22a0c50d31
SungHyun627 <kksscc4@gmail.com>
2021-06-29 17:57:53 +0900

    공

--- PS풀이저장소/구현/P1547/P1547.py
+++ PS풀이저장소/구현/P1547/P1547.py
@@ -8,7 +8,7 @@ n = int(stdin.readline().rstrip())
 # 볼의 위치
 ball_position = 1
 
-# n번의 cup의 위치 change
+## n번의 cup의 위치 change
 for _ in range(n):
     a, b = map(int, stdin.readline().rstrip().split())
     if ball_position == a:
