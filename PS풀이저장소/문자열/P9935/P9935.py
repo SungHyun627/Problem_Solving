fd21bd627efc985c5a6d0825854e29f6f9d9dae5
SungHyun627 <kksscc4@gmail.com>
2021-07-02 13:29:59 +0900

    문자열 폭발

--- PS풀이저장소/문자열/P9935/P9935.py
+++ PS풀이저장소/문자열/P9935/P9935.py
@@ -9,7 +9,7 @@ stack = []
 for i in range(len(s)):
     stack.append(s[i])
     
-    # Stack의 element 수가 bomb의 길이보다 같거나 클 때
+    ## Stack의 element 수가 bomb의 길이보다 같거나 클 때
     if len(stack) >= len(bomb):
         if ''.join(stack[-len(bomb):]) == bomb:
             for _ in range(len(bomb)):
