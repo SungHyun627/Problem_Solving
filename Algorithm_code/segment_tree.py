b89ef2fae46a878b07d4ee326c93f2cad4c3fe13
SungHyun627 <kksscc4@gmail.com>
2021-09-24 20:32:26 +0900

    구간 최소값 풀이 추가

--- Algorithm_code/segment_tree.py
+++ Algorithm_code/segment_tree.py
@@ -55,6 +55,18 @@ def find_interval_sum(node, start, end, left, right):
     mid = (start + end) // 2
     return find_interval_sum(node*2, start, mid, left, right) + find_interval_sum(node*2 + 1, mid+1, end, left, right)
 
+
+#구간 최소값 구하기
+def find_interval_min(node, start, end, left, right):
+    if right < start or left > end:
+        return int(1e9)
+    
+    if left <= start and right >= end:
+        return tree[node]
+    
+    mid = (start + end) // 2
+    return min(find_interval_min(node*2, start, mid, left, right), find_interval_min(node*2+1, mid+1, end, left, right))
+
 #트리 초기화
 init(1, 0, n-1)
 
