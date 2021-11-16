88a1e435d6965a3538762682120bfb52123513d2
SungHyun627 <kksscc4@gmail.com>
2021-11-16 20:03:06 +0900

    트리 자료구조

---
+++ Algorithm_code/tree.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, data, left_node, right_node):
+        self.data = data
+        self.left_node = left_node
+        self.right_node = right_node
+
+tree = {}
+
+# 전위 순회(Preorder Traversal)
+def pre_order(node):
+    print(node.data, end = ' ')
+    if node.left_node != None:
+        pre_order(tree[node.left_node])
+    if node.right_node != None:
+        pre_order(tree[node.right_node])
+
+#중위 순회(Inorder Traversal)
+def in_order(node):
+    if node.left_node != None:
+        in_order(tree[node.left_node])
+    print(node.data, end = ' ')
+    if node.left_node != None:
+        in_order(tree[node.right_node])
+
+#후위 순회(Postorder Traversal)
+def post_order(node):
+    if node.left_node != None:
+        post_order(tree[node.left_node])
+    if node.right_node != None:
+        post_order(tree[node.right_node])
+    print(node.data, end = ' ')
