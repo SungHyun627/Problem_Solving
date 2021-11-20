598d0c11529b519dd6ab8682d11705294e0532e3
SungHyun627 <kksscc4@gmail.com>
2021-11-20 17:06:17 +0900

    2×n 타일링 2

---
+++ PS풀이저장소/DP/P11727(2×n 타일링 2)/P11727.py
@@ -0,0 +1,12 @@
+#P11727 2×n 타일링 2
+from sys import stdin
+
+stdin = open('./input.txt', 'r')
+
+n = int(stdin.readline())
+dp = [0] * (n+1)
+dp[0], dp[1] = 1, 1
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + dp[i-2] * 2
+print(dp[n] % 10007)
