36b003ddc163bc05e19d3e8f43135e346253a917
Sunghyun627 <kksscc4@gmail.com>
2023-09-07 12:05:00 +0900

    최소 회의실 개수

---
+++ PS풀이저장소/Greedy/P19598(최소 회의실 개수)/P19598.py
@@ -0,0 +1,23 @@
+#P19598 최소 회의실 개수
+from sys import stdin
+import heapq
+stdin = open('./input.txt', 'r')
+
+n = int(stdin.readline())
+q = []
+arr = []
+result = 1
+
+for _ in range(n):
+  arr.append(list(map(int, stdin.readline().split())))
+arr.sort()
+heapq.heappush(q, arr[0][1])
+
+for i in range(1, n):
+  if arr[i][0] >= q[0]:
+    heapq.heappop(q)
+  else:
+    result += 1
+  heapq.heappush(q, arr[i][1])
+
+print(result)
