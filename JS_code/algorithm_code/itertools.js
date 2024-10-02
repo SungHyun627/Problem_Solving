a2a4a2549f57a0ef6adedf72b148afc8255f8dce
Sunghyun627 <kksscc4@gmail.com>
2024-10-02 13:29:19 +0900

    순열 조합 로직 수정

--- JS_code/algorithm_code/itertools.js
+++ JS_code/algorithm_code/itertools.js
@@ -1,7 +1,7 @@
 // 순열, 조합
 
 const getCombination = (arr, n) => {
-  if (n === 1) return arr.map((el) => [el]);
+  if (n === 1) return arr.map((el) => el);
   const result = [];
 
   arr.forEach((fixed, idx, origin) => {
@@ -17,15 +17,15 @@ const getCombination = (arr, n) => {
   return result;
 };
 
-const getPermutaion = (arr, n) => {
-  if (n === 1) return arr.map((el) => [el]);
+const getPermutation = (arr, n) => {
+  if (n === 1) return arr.map((el) => el);
   const result = [];
 
   arr.forEach((fixed, idx, origin) => {
     const rest = [...origin.slice(0, idx), ...origin.slice(idx + 1)];
     const perms = getPermutation(rest, n - 1);
     const attached = perms.map((perm) => {
-      if (n === 2) return fixed, perm;
+      if (n === 2) return [fixed, perm];
       return [fixed, ...perm];
     });
     result.push(...attached);
@@ -33,3 +33,8 @@ const getPermutaion = (arr, n) => {
 
   return result;
 };
+
+const arr = [1, 2, 3, 4];
+
+console.log(getCombination(arr, 2));
+console.log(getPermutation(arr, 2));
