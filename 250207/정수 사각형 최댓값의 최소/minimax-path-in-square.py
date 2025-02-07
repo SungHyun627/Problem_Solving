c3f83af383fc4cacff72f47b8d0121d04d26ed6a
SungHyun627 <62270427+SungHyun627@users.noreply.github.com>
2025-02-07 21:26:47 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 50ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250207/정수 사각형 최댓값의 최소/minimax-path-in-square.py
+++ 250207/정수 사각형 최댓값의 최소/minimax-path-in-square.py
@@ -1,17 +1,17 @@
 n = int(input())
-grid = [list(map(int, input().split())) for _ in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-graph = [[0] for _ in range(n+1)]
-dp = [[0]*(n+1) for _ in range(n+1)]
-graph[0].extend([0]*n)
+dp = [[0]*(n) for _ in range(n)]
 
-for i in range(1, n+1):
-    graph[i].extend(grid[i-1])
+dp[0] = [i for i in graph[0]]
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        dp[i][j] = max(grid[i-1][j-1], min(dp[i][j-1], dp[i-1][j]))
+for i in range(1, n):
+    dp[i][0] = graph[i][0]
 
-print(dp[n][n])
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(graph[i][j], min(dp[i][j-1], dp[i-1][j]))
+
+print(dp[n-1][n-1])
 
 
