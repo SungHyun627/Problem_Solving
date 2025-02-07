90fd43522d8b75888bde0bba2ee6b3e3eaa9b90d
SungHyun627 <62270427+SungHyun627@users.noreply.github.com>
2025-02-07 22:08:18 +0900

    Title: 중복되는 수열, Time: 128ms, Memory: 20MB, Status: Wrong Answer - CodeTree

---
+++ 250207/중복되는 수열/duplicate-sequence.py
@@ -0,0 +1,37 @@
+n = int(input())
+sequences = [input() for _ in range(n)]
+
+class Trie:
+    def __init__(self):
+        self.root = {}
+
+    def insert(self, s):
+        cur_node = self.root
+        for c in s:
+            if c not in cur_node:
+                cur_node[c] = {}
+                cur_node = cur_node[c]
+        cur_node['*'] = s
+
+    
+    def search(self, s):
+        cur_node = self.root
+        for c in s:
+            if c in cur_node:
+                cur_node = cur_node[c]
+            else:
+                return False
+        return True
+
+sequences.sort(lambda x: (len(x), x))
+
+def check():
+    t = Trie()
+    for i in range(n-1, -1, -1):
+        s = sequences[i]
+        if t.search(s):
+            return True
+        t.insert(s)
+    return False
+
+print(0 if check() else 1)
